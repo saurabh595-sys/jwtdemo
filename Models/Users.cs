c38890fa96f448ca203f3cc367d96a7b25599e4d
saurabhshetye <saurabhshetye18@gmail.com>
2022-05-19 11:11:53 +0530

    Add project files.

---
+++ Models/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace jwtdemo.Models
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ Repository/IJWTManagerRepository.cs
@@ -0,0 +1,13 @@
+﻿using jwtdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace jwtdemo.Repository
+{
+    public interface IJWTManagerRepository
+    {
+       Tokens Authenticate(Users users);
+       
+    }
+}
